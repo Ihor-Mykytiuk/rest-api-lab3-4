5d323241ed4f8b3b434da0e7437ded57d52fa519
Ihor Mykytiuk <igor14vn@gmail.com>
2025-03-22 20:35:13 +0200

    database configuration and Book model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
-from .routers import books
+from contextlib import asynccontextmanager
+from typing import Annotated
 
-app = FastAPI(title="Books API", version="1.0.0")
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Session, create_engine
+from .routers import books
+import os
+from dotenv import load_dotenv
+from .models import Book
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(title="Books API", version="1.0.0", lifespan=lifespan)
 
 app.include_router(books.router, prefix="/api/v1")
 
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+
+
+class Book(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+    author: str
+    published_year: int
+
