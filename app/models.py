65d21c0eb954b10c6605c9d70177dd2ac92eee02
Ihor Mykytiuk <igor14vn@gmail.com>
2025-03-23 17:49:11 +0200

    improve Book model structure + response models

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
+from datetime import datetime
+
 from sqlmodel import SQLModel, Field
 
 
-class Book(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    title: str
-    author: str
-    published_year: int
+class BookBase(SQLModel):
+    title: str = Field(..., max_length=100)
+    author: str = Field(..., max_length=255)
+    published_year: int = Field(..., ge=1000, le=datetime.now().year)
 
+
+class Book(BookBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class BookCreate(BookBase):
+    pass
--- app/routers/books.py
+++ app/routers/books.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Query
 
 from app.database import SessionDep
-from app.models import Book
+from app.models import Book, BookCreate
 
 router = APIRouter(
     prefix="/books",
@@ -20,27 +20,28 @@ async def get_books(
     return {"books": books, "limit": limit, "offset": offset}
 
 
-@router.get("/{book_id}")
+@router.get("/{book_id}", response_model=Book)
 async def get_book(book_id: int, session: SessionDep):
-    book = session.query(Book).get(book_id)
+    book = session.get(Book, book_id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
-    return {"book": book}
+    return book
 
 
-@router.post("/")
-async def add_book(book: Book, session: SessionDep):
-    session.add(book)
+@router.post("/", status_code=201, response_model=Book)
+async def add_book(book: BookCreate, session: SessionDep):
+    db_book = Book.model_validate(book)
+    session.add(db_book)
     session.commit()
-    session.refresh(book)
-    return {"book": book}
+    session.refresh(db_book)
+    return db_book
 
 
-@router.delete("/{book_id}")
+@router.delete("/{book_id}", status_code=204)
 async def delete_book(book_id: int, session: SessionDep):
     book = session.get(Book, book_id)
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     session.delete(book)
     session.commit()
-    return {"ok": True}
+    return
